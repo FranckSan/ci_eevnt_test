9599eb74ac47b3a80120f43ea0f659a26a9c6854
FranckSan <assalefranckhermanntanoh@gmail.com>
2022-01-04 17:42:33 +0000

    creation de route page acceuil en cours 1

---
+++ ci_event/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\Routes;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/',[Routes::class, 'index'])->name('acceuil');
+Route::get('/test',[Routes::class, 'test']);
